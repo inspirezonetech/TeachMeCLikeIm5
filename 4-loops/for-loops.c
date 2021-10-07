0daac708838898b8654c4cbfc2e1d1886e8d42b2
snehnakrani14 <88812207+snehnakrani14@users.noreply.github.com>
2021-10-06 21:21:19 -0700

    Update for-loops.c

--- 4-loops/for-loops.c
+++ 4-loops/for-loops.c
@@ -4,11 +4,23 @@
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+
+#include<stdio.h>
 int main()
 {
+    int limit,sum=0,loop;
+    
+    printf("Enter the Elements Limit for Sum : ");
+    scanf("%d",&limit);
+    
+    for (loop=1; loop<=limit ; loop++)
+    {
+        printf("%d\n",loop);
+        sum = sum + loop;
+    }
+    
+    printf("The Sum Of Elements are : %d",sum);
 }
-/*
 ------------------------------------------------------------------------------------
  Challenge: list challenges to be completed here. minimum of one challenge per tutorial
 ------------------------------------------------------------------------------------
