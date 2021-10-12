20b2387e0615711dffd46c114d23efac979583e0
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-10-12 10:52:31 +0530

    remove nested comment to avoid errors

--- 1-getting-started/commenting.c
+++ 1-getting-started/commenting.c
@@ -18,7 +18,7 @@ int main()
     int a = 5;
     /*
     I am a multiline comment
-    I start with '/*' and ends with '*/'
+    I can occupy any number of lines I need
     */
     printf("%d", a);  // We can also put comments onto side of a line like this
     return 0;  /* Even multiline comments can also be placed onto a side of a line */
