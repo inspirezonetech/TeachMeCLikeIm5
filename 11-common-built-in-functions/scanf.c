c41ee2bca0401d85e27fd6a01249560edd985a7c
Luis F. Talavera R <47088091+LuisFerTR@users.noreply.github.com>
2021-10-12 16:35:26 -0500

    Add scanf tutorial (#34)

--- 11-common-built-in-functions/scanf.c
+++ 11-common-built-in-functions/scanf.c
@@ -1,15 +1,61 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Using of scanf to read different data types.
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+// All functions related to standard input(read) and output(write)
+// are included in stdio.h.
+// stdio: STanDard Input and Output.
+#include <stdio.h>
+
+// Scanf is a function to read some data given by keyboard.
 int main()
 {
+    // Declare variables and initialize them.
+    char letter = 's';
+    int num = 100;
+    float decimal = 3.1415;
+
+    // Print them
+    printf("Initial values:\n");
+    printf("%c\n", letter);
+    printf("%d\n", num);
+    printf("%f\n", decimal);
+
+    // Modify its content through scanf
+    printf("Enter the new value of letter: ");
+    scanf("%c", &letter);
+
+    printf("Enter the new value of num: ");
+    scanf("%d", &num);
+
+    printf("Enter the new value of decimal: ");
+    scanf("%f", &decimal);
+
+    // As you can see scanf needs at least two parameters
+    // a format identifier (%d, %f, %c) and an & followed by the variable
+    // name (if you ARE using pointers, in that case you must
+    // ommit the "&").
+    // Format identifier tells to scanf the data type of the data you want
+    // to read.
+
+    // Print new values:
+    printf("New values:\n");
+    printf("%c\n", letter);
+    printf("%d\n", num);
+    printf("%f\n", decimal);
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Scanf can read multiple values at once, you just need to type the format identifiers 
+ between ", then type format identifiers separated by an space, that will be the first 
+ argument, after that, all the following arguments will be an & followed by 
+ the variable's name. If you want to read two int variables at once the syntax will be
+ scanf("%d %d", &num1, &num2)
+ 
+ Challenge: Read "Hello" letter by letter and print all the letters 
+            together. 
+
 ------------------------------------------------------------------------------------
 */
