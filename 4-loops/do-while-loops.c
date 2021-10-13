f3318a23369382c244bf2df3993c2afd96ac7b91
Henok Biadglign Ademtew <46082799+HenokB@users.noreply.github.com>
2021-10-13 18:41:13 -0400

    Update do-while-loops.c (#47)
    * Update do-while-loops.c
    
    * Update do-while-loops.c
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- 4-loops/do-while-loops.c
+++ 4-loops/do-while-loops.c
@@ -1,15 +1,37 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: Do while loop
+Unlike for and while loops, which test the loop condition at the top of the loop, the do...while loop in C programming checks its condition at the bottom of the loop.
+A do...while loop is similar to a while loop, except the fact that it is guaranteed to execute at least one time.
+ ------------------------------------------------------------------------------------
+ The general syntax for do-while-loops in C is: 
+ do {
+   statement(s);
+} while( condition );
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+// Example
+#include <stdio.h>
 int main()
-{
+{  // this is where the program start execution
+    // local variable declaration
+    int number = 2;
+    // do loop execution
+    do
+    {
+        printf("The number is: %d\n", number);
+        // increment in every step
+        number = number + 1;
+        // while loop execution
+    } while (number < 5);
+    return 0;
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenges:
+ Using a do while loop:
+ 1. Print all the numbers from 1 to 10.
+ 2. Print all even numbers up to 5.
 ------------------------------------------------------------------------------------
 */
