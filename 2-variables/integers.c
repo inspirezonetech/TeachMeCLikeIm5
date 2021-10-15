ed3bc7d3ae2a8c128eac9c4a47b18fda495022dd
Ruan Dias <ruan_ndias@hotmail.com>
2021-10-15 17:31:59 -0300

    added integer C data type tutorial and challenge (#51)
    * added integer C data type tutorial and challenge
    
    * Run Lint with cpplint!
    
    * running Lint with cpplint again (Laughing Emoji)
    
    * Update integers.c
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- 2-variables/integers.c
+++ 2-variables/integers.c
@@ -1,15 +1,25 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: int (integer) is one of the data types in C programming.
+ The size of int is at least 2, usually 4 bytes.
+ Integers are whole numbers -> zero, positive and negative values but no decimal values. 
+ For example, 0, -3, 11.
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+#include <stdio.h>
 int main()
 {
+  int age;  // declaring a variable
+
+  age = 27;  // assigning value to an integer variable
+  printf("My name is Ruan and I'm %d years old.\n", age);
+
+  return 0;
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: Read two integer values, in this case for variables 'a' and 'b'. Then calculate the sum 
+ between them and assign it to a variable called 'sum'. Then print the value of the 'sum' variable.
 ------------------------------------------------------------------------------------
 */
