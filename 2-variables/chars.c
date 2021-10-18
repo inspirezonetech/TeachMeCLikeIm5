4c34a96a51f63f2d0a4aa988e68bc4f5f8b6ed1d
Ruan Dias <avanade.ruan@grupofleury.com.br>
2021-10-18 16:01:51 -0300

    chars.c tutorial and challenge. (#56)

--- 2-variables/chars.c
+++ 2-variables/chars.c
@@ -1,15 +1,33 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+C uses char type to store characters and letters. 
+But, underneath, C stores integer numbers instead of characters.
+Char values are stored in 1 byte in memory,and value range from -128 to 127 or 0 to 255.
+In order to represent characters, the computer has to map each integer 
+with a corresponding character using a numerical code. 
+The most common numerical code is ASCII.
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
+#include <stdio.h>
 int main()
 {
+    char character = 'R';
+    printf("character = %c, Stored as char\n", character);
+    printf("character = %d, Stored as integer\n", character);
+    return 0;
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge:
+ 01 - Make a program that asks: "Do you are human ? ('y'or 'n')" and catch the answer on a char variable. 
+ Then, print on the console the answer.
+
+02 - Make a program that reads a character and tells you what class that character belongs to.
+Consider the following classes: digit (characters from `0' to `9'); 
+capital letters (characters from `A' to `Z'); 
+lowercase letters (characters from `a' to `z') ; 
+others (any others characters).
+Tip: Check the ASCII codes for each character class. 
 ------------------------------------------------------------------------------------
 */
