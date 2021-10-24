514b1ed232dc1811caf0e61e9420814f99e123ea
Manan Uppadhyay <mananuppadhyay@gmail.com>
2021-10-25 04:48:18 +0530

    added compiling-programs.c

--- 1-getting-started/compiling-programs.c
+++ 1-getting-started/compiling-programs.c
@@ -1,15 +1,34 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+   Tutorial: Compiling a C program using gcc
+    Compiling refers to the translation of source code into object code using a compiler.
+    In this tutorial we'll get to know about gcc, which is the abbreviation gor GNU compiler collection.
+    We'll be using command prompt to compile our C program.
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
-int main()
-{
-}
-/*
-------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+/* ------------------------------------------------------------------------------------
+    Installing gcc:
+    1. Open command prompt, by searching for the same in the search bar and type gcc -v and press Enter.
+    2. If the command prompt shows you the version for gcc, you are good to go, else you;ll need to install gcc, from their official website.
+
+
+    Compiling the program:
+    1. Open the command prompt and using the cd command change the directory to the location of your C program.
+    Alternatively, you can go to the location of your program and on the Address bar and type cmd. It takes you to the command prompt with the current folder as the working directory.
+    2. Type the command: gcc <name of your file>.c
+    Eg: gcc palindrome.c
+    3. The output is on your screen now!
+
 ------------------------------------------------------------------------------------
 */
+
+// Challenge:
+#include <stdio.h>
+int main()
+{
+    printf("Hello, World!");
+    return 0;
+}
+
+// Find out the output of the above written program via the gcc compiler method explained above.
