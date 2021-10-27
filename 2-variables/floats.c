2f3692a3f1f8473b61f44d96a33230d7d55641fc
ruandias <ruan_ndias@hotmail.com>
2021-10-26 21:32:00 -0300

    floats.c tutorial and challenge.

--- 2-variables/floats.c
+++ 2-variables/floats.c
@@ -1,15 +1,26 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: float is a datatype which is used to represent the floating point numbers.
+ It has 6 decimal digits of precision.
+ For example, 10.327, -3.14, 11.234.
 ------------------------------------------------------------------------------------
 */
 
 // Code here explaining concept with comments to guide
-int main()
-{
+#include<stdio.h>
+#include<string.h>
+int main() {
+  float x = 50.621;
+  int y = 40;
+  printf("The float value : %f\n", x);
+  printf("The sum of float and int variable : %f\n", (x+y));
+  return 0;
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: Make a program that reads a salesperson's name, 
+ his fixed salary and the total sales made by him in the month (in cash).
+ Knowing that this seller earns 15% commission on his sales made,
+ inform the total receivable at the end of the month, with two decimal places.
 ------------------------------------------------------------------------------------
 */
