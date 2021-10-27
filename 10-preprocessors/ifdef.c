363385f67228fb9dd50e12e3e4e4d737ffe3b8a4
Saul <SaulMiquilena@users.noreply.github.com>
2021-10-27 18:18:51 -0400

    tutorial-ifdef.c (#66)
    * tutorial-ifdef.c
    
    * Update ifdef.c
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- 10-preprocessors/ifdef.c
+++ 10-preprocessors/ifdef.c
@@ -1,15 +1,41 @@
 /* 
 ------------------------------------------------------------------------------------
- Tutorial: brief description of tutorial content
+ Tutorial: #ifdef (if defined)
 ------------------------------------------------------------------------------------
 */
 
-// Code here explaining concept with comments to guide
-int main()
+/*
+We can check if a symbolic constant or a macro is defined using #ifdef
+We can also use #if defined to do the same task.
+*/
+
+#include <stdio.h>
+#define VALUE 1
+
+int main(void)
 {
+    #ifdef VALUE
+        printf("Value is defined\n");
+    #else
+        printf("Value is not defined\n");
+    #endif
 }
 /*
 ------------------------------------------------------------------------------------
- Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+ Challenge: What does the following code output
+ 
+ #include <stdio.h>
+ #define UNIX 1
+
+ int main()
+ {
+    #ifdef UNIX
+        printf("UNIX specific function calls go here.\n");
+    #endif
+
+    printf("TechOnTheNet is over 10 years old.\n");
+
+    return 0;
+ }
 ------------------------------------------------------------------------------------
 */
