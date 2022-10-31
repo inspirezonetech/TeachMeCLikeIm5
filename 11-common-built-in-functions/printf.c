378e11d3e4c9f4a67ac483330c3c5a3b152c6441
Fum <66768334+funbeedev@users.noreply.github.com>
2022-10-31 14:56:37 +0000

    Tidy up tutorial format

--- 11-common-built-in-functions/printf.c
+++ 11-common-built-in-functions/printf.c
@@ -11,36 +11,47 @@
            - printf function returns the number of characters printed and negative if error occurs
 -----------------------------------------------------------------------------------------------------------------------------------
 */
-#include <stdio.h>   // The #include stdio.h> declaration must be used to include
+
+#include <stdio.h>   // The "#include stdio.h>"" declaration must be used to include
                     // the stdio.h header file if we want to utilise printf().
+
 int main() {  // The main function
+
   // print whole string using double quotation marks ("").
   printf("Hello World \n");  // \n is used for line break
+
   // you can also print the number of element in print function
   int x = printf("Hi github");
-  printf("\n %d \n", x);  // 9 is the output
+  printf("\n %d \n", x);  // 9 is the output (note: the space character is counted)
+
   // Declaring variables
   int a = 10;
   char c = 'c';
   float f = 12.3;
   double d = 15.43;
+
   // For printing int types, we utilise the %d format specifier. The value
   // of the variable is substituted for by %d.
   printf("Integer a = %d \n", a);
+
   // To print char type, %c is used.
   printf("Character c = %c \n", c);
+
   // To print float, we use %f format specifier.
   printf("Float f = %f \n", f);
+
   // To print double type, %lf is used.
   printf("Double d = %lf \n", d);
+
   // You can do some operation printf function and result will be displayed.
   printf("a + d = %lf \n", a+d);
+
   return 0;
 }
 /*
 --------------------------------------------------------------------------------------------------------------------------------------
-Challange: Take input from user in variable a and b, print number of elements present in each variable and then print sum of number of
-            elements presenta and b.
+Challenge: Take input from user in variable a and b, print number of elements present in each variable and then print sum of number of
+            elements present in a and b.
             example: 
               - a = 12343212 and b = 887788
               - number of elements in a is 8 and in b is 6.
